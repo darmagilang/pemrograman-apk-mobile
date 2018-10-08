ea3643d3275ac81a136b1626e9c768a5bf3057d1
darmagilang <42999018+darmagilang@users.noreply.github.com>
2018-10-08 17:02:11 +0700

    Add files via upload

---
+++ src/main/java/com/example/gilang/tugaskuis/B1.java
@@ -0,0 +1,17 @@
+package com.example.gilang.tugaskuis;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.View;
+
+public class B1 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b1);
+        {public void bold(View;)
+            Intent intent = onNewIntent( MainActivity.this, Main2Activity.class)
+            startActivity(intent);
+}
---
+++ src/main/java/com/example/gilang/tugaskuis/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gilang.tugaskuis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Bold(View view) {getIntent(createPackageContext: B1.this, B1.class)StartActivity(intent)
+
+    }
+}
